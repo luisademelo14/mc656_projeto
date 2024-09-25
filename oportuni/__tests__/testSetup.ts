b242bf80744fe868271a25a0937f2140c9c37fd4
Letícia <l184423@dac.unicamp.br>
2024-09-25 09:05:04 -0300

    comentários em testSetup.ts

--- oportuni/__tests__/testSetup.ts
+++ oportuni/__tests__/testSetup.ts
@@ -4,14 +4,14 @@ const httpMocks = require('node-mocks-http');
 const User = require('@/models/User');
 const bcrypt = require('bcryptjs');
 
-jest.mock('@/lib/mongodb'); // mocking the dbConnect function
-jest.mock('@/models/User', () => {
+jest.mock('@/lib/mongodb');         // mocking the dbConnect function
+jest.mock('@/models/User', () => {  // mocking the User model
   const mockUser = jest.fn();
   return mockUser;
 });
 
-User.findOne = jest.fn(); // mocking the findOne method
-User.prototype.save = jest.fn(); // mocking the save method
+User.findOne = jest.fn();         // mocking the findOne method
+User.prototype.save = jest.fn();  // mocking the save method
 
 export const createRequestResponse = (method: string, body: object) => {
   return httpMocks.createMocks({
@@ -20,5 +20,4 @@ export const createRequestResponse = (method: string, body: object) => {
   });
 };
 
-// Exportar outros métodos ou variáveis comuns que você precise
 export { httpMocks, bcrypt, User };
