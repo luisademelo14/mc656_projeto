0dfc5898d0c2de8d3a1a5e621fd776a35effb8fa
Igor Batista <igorengbatista@gmail.com>
2024-10-29 10:18:59 -0300

    ajuste página de busca

--- oportuni/src/app/pages/search/page.tsx
+++ oportuni/src/app/pages/search/page.tsx
@@ -1,15 +1,23 @@
 "use client"
 import Footer from '@/src/components/Footer';
+import Box from '@mui/material/Box';
+import React from 'react';
+import SearchBar from '@/src/components/SearchBar'; 
+import "./styles.css"
 
 const Search = () => {
+  const [valorDaBusca, setValorDaBusca] = React.useState<string>("ç~");
   return (
-    <div className="flex flex-col justify-between min-h-screen w-full bg-white">
+    <Box>
+      <SearchBar onSearch={(query: string) => setValorDaBusca(query)} />
+    <Box className="flex flex-col justify-between min-h-screen w-full bg-white">
       <main className="flex-grow flex items-center justify-center">
         <h1 className="text-4xl font-bold">Bem-vindo à Pesquisa</h1>
       </main>
 
       <Footer /> {/* Reusable Footer component */}
-    </div>
+    </Box>
+  </Box>
   );
 };
 
