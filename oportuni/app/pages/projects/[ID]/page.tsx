dd4d098f67a333493ca2b43017c8fb9427997190
luisademelo14 <l238001@dac.unicamp.br>
2024-11-02 16:45:42 -0300

    botoes funcionam agora. falta verificar se os caminhos de redirecionamento ficaram certos

--- oportuni/app/pages/projects/[ID]/page.tsx
+++ oportuni/app/pages/projects/[ID]/page.tsx
@@ -2,6 +2,8 @@
 import dbConnect from '@/lib/mongodb';
 import Project, { IProject } from '@/models/Project';
 import { notFound } from 'next/navigation';
+import { Box } from '@mui/material';
+import InteractiveButtons from './InteractiveButtons';
 
 interface ProjectPageProps {
   params: { ID: string };
@@ -14,12 +16,19 @@ const ProjectPage = async ({ params }: ProjectPageProps) => {
   const project: IProject | null = await Project.findOne({ ID: Number(ID) }).lean();
 
   if (!project) {
-    console.log('Project not found');
+    console.log('Projeto não encontrado');
     notFound(); // Redireciona para uma página 404 se o projeto não for encontrado
   }
 
   return (
-    <div className="flex flex-col items-center min-h-screen p-6 bg-gray-100">
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      minHeight="100vh"
+      p={6}
+      sx={{ backgroundColor: 'gray.100' }}
+    >
       <h1 className="text-3xl font-bold mb-4">{project.name}</h1>
       <img 
         src={project.imageUrl} 
@@ -27,15 +36,18 @@ const ProjectPage = async ({ params }: ProjectPageProps) => {
         className="w-64 h-64 object-cover rounded-md mb-4" 
       />
       <p className="text-lg text-gray-700">{project.description}</p>
-      <div className="mt-4">
+      <Box mt={4}>
         <p><strong>Data de Início:</strong> {new Date(project.startDate).toLocaleDateString()}</p>
         <p><strong>Idade Mínima:</strong> {project.minAge}</p>
         <p><strong>Categoria:</strong> {project.category}</p>
         <p><strong>Certificação:</strong> {project.certification}</p>
         <p><strong>Nível Educacional:</strong> {project.educationLevel}</p>
         <p><strong>Tópicos:</strong> {project.topics.join(', ')}</p>
-      </div>
-    </div>
+      </Box>
+
+      {/* Botões interativos */}
+      <InteractiveButtons />
+    </Box>
   );
 };
 
