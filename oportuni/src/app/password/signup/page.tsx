b4f662d877b9b38052a7020a94fd804a325c6a80
Letícia <l184423@dac.unicamp.br>
2024-11-09 17:02:33 -0300

    Padronizando páginas de login e recovery

--- oportuni/src/app/password/signup/page.tsx
+++ oportuni/src/app/password/signup/page.tsx
@@ -28,23 +28,24 @@ const Signup: React.FC = () => {
 
   return (
     <Box
-      alignItems="center"     // Centraliza verticalmente
-      justifyContent="center" // Centraliza horizontalmente
+      alignItems="center"
+      justifyContent="center"
       display="flex"
       sx={{
         backgroundImage: 'url(/imagens/fundo.jpg)',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         minHeight: '100vh',
-        minWidth: '100vw',
-        padding: 2,
+        width: '100vw',
+        flexDirection: 'column',
+        overflow: 'auto',
       }}
     >
-      <Box 
-        bgcolor="#FEFFEE" 
+      <Box
+        bgcolor="#FEFFEE"
         borderRadius="16px"
-        maxWidth={'400px'}
-        width={'100%'}
+        maxWidth="400px"
+        width="100%"
         maxHeight="100vh"
         display="grid"
         className="default-text bold-text large-text"
@@ -52,31 +53,24 @@ const Signup: React.FC = () => {
       >
         {isSuccessful ? (
           <Box display="flex" justifyContent="center" alignItems="center">
-            <p>
-              Bem vindo!
-            </p>
+            <p>Bem-vindo!</p>
           </Box>
         ) : (
           <>
-            <AuthForm 
-              mode="Signup" 
-              onSubmit={handleSignup}
-            />
-
+            <AuthForm mode="Signup" onSubmit={handleSignup} />
             {message && (
               <p
                 className={`text-center mt-4 ${
-                  isSuccess ? "text-red-500" : "text-green-800" 
+                  isSuccess ? "text-green-800" : "text-red-500"
                 }`}
-                style={{ fontSize: '20px' }}  // Ajuste de fonte na mensagem de sucesso ou erro
+                style={{ fontSize: '20px' }}
               >
                 {message}
               </p>
             )}
-
             <Box className="default-text small-text center-text" marginTop="20px">
               <Link href="/password/login">
-                <h1 style={{ color: '#036564', marginBottom: '8px'}}> {/* Aplicação da cor personalizada */}
+                <h1 style={{ color: '#036564', marginBottom: '8px' }}>
                   Voltar para Login
                 </h1>
               </Link>
