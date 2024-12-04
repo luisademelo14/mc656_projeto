731cfc0ca205b18e2bcc112de1d136e342e81603
Igor Batista <igorengbatista@gmail.com>
2024-12-03 21:11:21 -0300

    Exibição de favoritos corrigida

--- oportuni/src/pages/api/project/favorites.ts
+++ oportuni/src/pages/api/project/favorites.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "@/src/lib/mongodb";
-import Favorite, { IFavorite } from "@/src/models/Favorite";
+import User from "@/src/models/User";
 import Project from "@/src/models/Project";
 
 // Função para buscar os projetos favoritados pelo usuário
@@ -8,15 +8,18 @@ const getFavoritesByUserId = async (userId: string, limit = 10) => {
   await dbConnect();
   try {
     console.log("Buscando favoritos para o usuário:", userId);
+    const user = await User.findById(userId);
 
-    const favoriteEntries = await Favorite.find({ userId }).lean<IFavorite[]>();
+    if (!user) throw new Error("Usuário não encontrado");
+    const favoriteEntries = user.favorites;
+
     console.log("Favoritos encontrados:", favoriteEntries);
 
     if (favoriteEntries.length === 0) {
       return [];
     }
 
-    const favoriteProjectIds = favoriteEntries.map((fav) => fav.projectId);
+    const favoriteProjectIds = favoriteEntries.map((fav) => fav);
     console.log("IDs dos projetos favoritos:", favoriteProjectIds);
 
     const projects = await Project.find({ ID: { $in: favoriteProjectIds } })
@@ -39,6 +42,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === "GET") {
     // Obtém o userId do cookie
     const userId = req.cookies.userId;
+    console.log("ID do usuário:", userId);
 
     if (!userId) {
       return res.status(401).json({ message: "Usuário não autenticado." });
